0729fcffc14e99785926977df2dcc4b7bfa2290e
Aniket Datta <datta.aniket7@gmail.com>
2021-11-27 20:31:38 +0530

    cstock

---
+++ cstock.py
@@ -0,0 +1,12 @@
+for i in range(int(input())):
+    s,a,b,c=map(int,input().split())
+    c=s+s*(c/100)
+    if c>=a and c<=b:
+        print("YES")
+    else:
+        print("NO") 
+        
+# 3
+# 100 93 108 7
+# 100 94 100 -7
+# 183 152 172 -17
